1eb690b391102f64871ceea6d5790facde56e75e
Andrea Golin <andreagolin.85@gmail.com>
2020-06-30 17:54:25 +0200

    First call for testing

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <EvesnipMain></EvesnipMain>
   </div>
 </template>
@@ -16,7 +15,11 @@ export default {
 }
 </script>
 
-<style>
+
+<style lang="scss">
+@import "node_modules/bootstrap/scss/bootstrap";
+@import "node_modules/bootstrap-vue/src/index.scss";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- frontend/src/components/EvesnipMain.vue
+++ frontend/src/components/EvesnipMain.vue
@@ -1,5 +1,27 @@
 <template>
     <div class="col-md-12">
         <h2>Main</h2>
+        <b-button variant="success" @click="getStatus">Button</b-button>
+        <div>
+            <p>Active now: {{ status.players }}</p>
+        </div>
     </div>
 </template>
+
+<script>
+    export default{
+        name: "Evesnip Main",
+        data() {
+            return {
+                status: ''
+            }
+        },
+        methods: {
+            getStatus: function(){
+                this.$http.get("https://esi.evetech.net/latest/status/?datasource=tranquility").then(response => {
+                    this.status = response.data;
+                });
+            }
+        }
+     };
+</script>
